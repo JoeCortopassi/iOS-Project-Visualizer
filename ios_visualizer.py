b25b4649c02d71b579489a80205914090ce78452
Joe Cortopassi <joe@joecortopassi.com>
2014-07-12 21:11:01 -0700

    Node size adjusts based on file size

--- ios_visualizer.py
+++ ios_visualizer.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 rootdir = sys.argv[1]
 
 outputOfFiles = ""
+outputOfFileSize = {}
 
 for root, subFolders, files in os.walk(rootdir):
     for filename in files:
@@ -17,18 +18,32 @@ for root, subFolders, files in os.walk(rootdir):
             
             with open( filePath, 'r' ) as f:
                 thisFilesContents = f.read()
+                clean_filename = filename.split('.')[0]
+                outputOfFileSize[clean_filename] = len(thisFilesContents)
                 for item in thisFilesContents.split("\n"):
                     if "#import" in item and "<UIKit/UIKit.h>" not in item and "<" not in item:
-                        #        print "~~ %s" % (item.strip())
-                        clean_filename = filename.split('.')[0]
                         clean_import = item.split('"')[1].split('.')[0]
                         outputOfFiles += "%s\t%s\n" % (clean_filename, clean_import)
 
-print(outputOfFiles)
+#print(outputOfFiles)
 
 e1=cStringIO.StringIO(outputOfFiles)
 G1=nx.read_edgelist(e1,delimiter='\t')
-pos=nx.spring_layout(G1,iterations=100)
+d = nx.degree(G1)
+#print("\n")
+#print(d.keys())
+#print("\n")
+node_sizes = []
+for fileKey in d.keys():
+    size_of_node = 1
+    if fileKey in outputOfFileSize.keys():
+        size_of_node = outputOfFileSize[fileKey]
+    node_sizes.append(size_of_node)
+#print("\n")
+#print(node_sizes)
+#print("\n")
+
+pos=nx.spring_layout(G1,k=1,iterations=20)
 plt.title(os.path.basename(os.path.normpath(rootdir)))
-nx.draw(G1,pos,node_size=50,with_labels=True)
+nx.draw(G1,pos,with_labels=True, node_size=node_sizes)
 plt.savefig("visualized_ios_project.png") # save as png
